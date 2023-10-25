44507bc2a639de3f6b1f837112cc3bbab2db10bf
Giulia Silvestrini <silvergiuly@hotmail.it>
2023-10-25 17:39:08 +0200

    toggle funzionante

--- src/Components/SingleBook.jsx
+++ src/Components/SingleBook.jsx
@@ -3,35 +3,39 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import { Component } from "react";
 
-
-
-
-
 class SingleBook extends Component {
   state = { selected: false };
+
+  Clickedbook = () => {
+    console.log("clicckato");
+    if (this.state.selected === false) {
+      this.setState({ selected: true });
+    } else {
+      this.setState({ selected: false });
+    }
+  };
+
   render() {
     return (
       <Col sm={12} md={6} lg={3} key={this.props.Libro.asin} className=" p-2">
         <Card className="h-100 ">
           <Card.Img
+            onClick={this.Clickedbook}
             src={this.props.Libro.img}
-            className="bg-warning-subtle p-3 h-75"
+            className={` p-3 h-75 ${
+              this.state.selected ? "bg-danger" : "bg-warning-subtle"
+            }`}
           />
-          <Card.Body className="bg-warning-subtle ">
+          <Card.Body className="bg-warning-subtle  ">
             <Card.Title>
               <p>{this.props.Libro.title}</p>
             </Card.Title>
             <Card.Text>Categoria: "{this.props.Libro.category}"</Card.Text>
-            <Button onClick={ClickedBook} variant="success">€ {this.props.Libro.price}</Button>
+            <Button variant="success">€ {this.props.Libro.price}</Button>
           </Card.Body>
         </Card>
       </Col>
     );
   }
 }
-
-function ClickedBook (e){
-if ()
-}
-
 export default SingleBook;
